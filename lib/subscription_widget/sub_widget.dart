7db4880c415c9206fdcfb57800f15cdf0258c841
Dmitry Zayats <zayatsdmitry93@gmail.com>
2022-10-08 01:37:06 +0300

    Words build

--- lib/subscription_widget/sub_widget.dart
+++ lib/subscription_widget/sub_widget.dart
@@ -1,7 +1,4 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class SubscribeWidget extends StatefulWidget {
   const SubscribeWidget({Key? key}) : super(key: key);
@@ -15,36 +12,22 @@ class _SubscribeWidgetState extends State<SubscribeWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xff141733),
-      body: SafeArea(
-        left: false,
-        right: false,
-        top: false,
-        bottom: false,
-        child: Stack(
-          children: [
-            Expanded(
-              child: Image.asset("assets/images/Group.png",
-                  width: MediaQuery.of(context).size.width),
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              child: Expanded(
-                child: Image.asset(
-                  "assets/images/Group.png",
-                ),
-              ),
-              color: Colors.red,
-            ),
-            Column(
-              children: const [
-                // CloseButton(),
-                MoonWidget(),
-                // SheepMoonWidget(),
-                // CloudOne(),
-              ],
-            ),
-          ],
-        ),
+      body: Stack(
+        children: [
+          Image.asset(
+            "assets/images/shopping/Stars.png",
+            width: double.infinity,
+            fit: BoxFit.cover,
+            alignment: Alignment.center,
+          ),
+          Column(
+            children: const [
+              CloseButton(),
+              MoonWidget(),
+              WordsWidget(),
+            ],
+          ),
+        ],
       ),
     );
   }
@@ -56,7 +39,6 @@ class CloseButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        // margin: EdgeInsets.all(40),
         alignment: Alignment.topRight,
         padding: const EdgeInsets.only(right: 10, top: 25),
         child: IconButton(
@@ -80,78 +62,81 @@ class _MoonWidgetState extends State<MoonWidget> {
   @override
   Widget build(BuildContext context) {
     return Expanded(
+      flex: 1,
       child: Stack(
         children: [
           Positioned(
             top: 105,
             right: 58,
-            child: Image.asset("assets/images/sheep.png"),
+            child: Image.asset("assets/images/shopping/Sheep.png"),
           ),
           Positioned(
             left: MediaQuery.of(context).size.width / 2 - 126,
             top: 82,
             child: Image.asset(
-              "assets/images/Group 60.png",
+              "assets/images/shopping/Moon.png",
               // color: Colors.red,
             ),
           ),
           Positioned(
             top: 197,
-            right: 205,
-            child: Image.asset("assets/images/150.png"),
+            left: 65,
+            child: Image.asset(
+              "assets/images/shopping/Leftbottomcloud.png",
+            ),
           ),
           Positioned(
             top: 105,
             right: 0,
-            child: Image.asset("assets/images/151.png"),
+            child: Image.asset("assets/images/shopping/Rightbottomcloud.png"),
           ),
           Positioned(
             top: 103,
             left: 0,
-            child: Image.asset("assets/images/153.png"),
+            child: Image.asset("assets/images/shopping/Lefttopcloud.png"),
           ),
         ],
       ),
     );
-    // color: Colors.black,
-    // alignment: Alignment.topCenter,
-    // child: Image.asset(
-    //   "assets/images/Group 60.png",
-    //   width: 150,
-    //   height: 150,
-    //   ),
-    // );
   }
 }
 
-class SheepMoonWidget extends StatelessWidget {
-  const SheepMoonWidget({super.key});
+class WordsWidget extends StatelessWidget {
+  const WordsWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: Colors.black,
-        alignment: Alignment.topCenter,
-        child: Image.asset(
-          "assets/images/sheep.png",
-          width: 150,
-          height: 150,
-        ));
-  }
-}
-
-class CloudOne extends StatelessWidget {
-  const CloudOne({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.topLeft,
-      color: Colors.black,
-      child: Image.asset(
-        "assets/images/150.png",
-        width: 150,
-        height: 150,
+    return Expanded(
+      flex: 2,
+      child: Container(
+        // color: Colors.red,
+        alignment: Alignment.bottomCenter,
+        child: Column(
+          textDirection: TextDirection.ltr,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: const <Widget>[
+            Text(
+              "Sweet dreams",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 33,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            Text(
+              "PREMIUM",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 33,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            SizedBox(
+              height: 44,
+            ),
+          ],
+        ),
       ),
     );
   }

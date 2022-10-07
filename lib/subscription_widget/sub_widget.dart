183c66535e6085f5bea00e1f905d4eabec7fc67d
Dmitry Zayats <zayatsdmitry93@gmail.com>
2022-10-07 20:20:25 +0300

    add images

--- lib/subscription_widget/sub_widget.dart
+++ lib/subscription_widget/sub_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class SubscribeWidget extends StatefulWidget {
@@ -12,27 +14,87 @@ class _SubscribeWidgetState extends State<SubscribeWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        MoonWidget();
+        children: const [
+          CloseButton(),
+          MoonWidget(),
+          SheepMoonWidget(),
+          CloudOne(),
+        ],
       ),
     );
   }
 }
 
+class CloseButton extends StatelessWidget {
+  const CloseButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        // margin: EdgeInsets.all(40),
+        alignment: Alignment.topRight,
+        padding: const EdgeInsets.only(right: 10, top: 25),
+        child: IconButton(
+          icon: const Icon(
+            Icons.close,
+            color: Colors.white,
+          ),
+          onPressed: () => null,
+        ));
+  }
+}
+
 class MoonWidget extends StatefulWidget {
   const MoonWidget({super.key});
+
   @override
   State<MoonWidget> createState() => _MoonWidgetState();
 }
+
 class _MoonWidgetState extends State<MoonWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
-          padding: const EdgeInsets.all(50),
-          alignment: Alignment.topRight,
-          child: const Icon(
-            Icons.close,
-            color: Colors.black,
-          ),
-        );
+      color: Colors.black,
+      alignment: Alignment.topCenter,
+      child: Image.asset(
+        "assets/images/Group 60.png",
+        width: 150,
+        height: 150,
+      ),
+    );
+  }
+}
+
+class SheepMoonWidget extends StatelessWidget {
+  const SheepMoonWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.black,
+        alignment: Alignment.topCenter,
+        child: Image.asset(
+          "assets/images/sheep.png",
+          width: 150,
+          height: 150,
+        ));
+  }
+}
+
+class CloudOne extends StatelessWidget {
+  const CloudOne({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.topLeft,
+      color: Colors.black,
+      child: Image.asset(
+        "assets/images/150.png",
+        width: 150,
+        height: 150,
+      ),
+    );
   }
 }

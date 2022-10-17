e6fd477c7e24d190e95b95cf00028816509e8422
Dmitry Zayats <zayatsdmitry93@gmail.com>
2022-10-17 20:49:13 +0300

    Alex route work

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'boarding.dart';
+
+import 'onboardingalex.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const BoardingWidget(),
+      home: const OnBoardingWidget(),
     );
   }
 }
--- lib/subscription_widget/sub_widget.dart
+++ lib/subscription_widget/sub_widget.dart
@@ -24,8 +24,8 @@ class _SubscribeWidgetState extends State<SubscribeWidget> {
           Column(
             children: const [
               CloseButton(),
-              // MoonWidget(),
-              // AboutPremiumWidget(),
+              MoonWidget(),
+              AboutPremiumWidget(),
             ],
           ),
         ],

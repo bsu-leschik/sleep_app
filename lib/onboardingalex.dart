b96047a35fbb5a1931a10d71b53a00f448040f7d
Dmitry Zayats <zayatsdmitry93@gmail.com>
2022-10-18 17:02:47 +0300

    Refactoring code

--- lib/onboardingalex.dart
+++ lib/onboardingalex.dart
@@ -140,63 +140,46 @@ class _OnBoardingWidgetState extends State<OnBoardingWidget> {
                     const SizedBox(
                       height: 50,
                     ),
-                    Center(
-                      child: SmoothPageIndicator(
-                        controller: _pageController,
-                        count: 3,
-                        effect: const WormEffect(
-                          dotColor: Colors.white,
-                          activeDotColor: Colors.amber,
-                          dotHeight: 10.0,
-                          dotWidth: 10.0,
-                          spacing: 10.0,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 224,
-                    ),
-                    Positioned(
-                      top: 662,
-                      left: 65,
-                      child: CupertinoButton(
-                        onPressed: () {
-                          if (_pageController.page == 1) {}
-
-                          if (_pageController.page == 2) {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      const SubscribeWidget()),
-                            );
-                          } else {
-                            _pageController.nextPage(
-                              duration: const Duration(
-                                milliseconds: 400,
-                              ),
-                              curve: Curves.ease,
-                            );
-                          }
-                        },
-                        minSize: 74.0,
-                        color: const Color.fromARGB(255, 4, 95, 227),
-                        borderRadius: BorderRadius.circular(45.0),
-                        child: const Text(
-                          "Continue",
-                          style: TextStyle(
-                            fontFamily: 'SFProDisplay',
-                            color: Color(0xffffffff),
-                            fontSize: 20,
-                            fontWeight: FontWeight.w700,
-                            fontStyle: FontStyle.normal,
-                          ),
-                        ),
-                      ),
-                    ),
                   ],
                 ),
               ],
+            ),
+            Positioned(
+              top: 662,
+              right: 85,
+              child: CupertinoButton(
+                onPressed: () {
+                  if (_pageController.page == 1) {}
+
+                  if (_pageController.page == 2) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const SubscribeWidget()),
+                    );
+                  } else {
+                    _pageController.nextPage(
+                      duration: const Duration(
+                        milliseconds: 400,
+                      ),
+                      curve: Curves.ease,
+                    );
+                  }
+                },
+                minSize: 74.0,
+                color: const Color.fromARGB(255, 4, 95, 227),
+                borderRadius: BorderRadius.circular(45.0),
+                child: const Text(
+                  "Continue",
+                  style: TextStyle(
+                    fontFamily: 'SFProDisplay',
+                    color: Color(0xffffffff),
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                    fontStyle: FontStyle.normal,
+                  ),
+                ),
+              ),
             ),
           ],
         ),

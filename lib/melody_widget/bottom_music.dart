da069b76c901e5c516743e8522e9374efc02e2a0
Dmitry Zayats <zayatsdmitry93@gmail.com>
2022-11-23 11:13:22 +0300

    Cicle widget

--- lib/melody_widget/bottom_music.dart
+++ lib/melody_widget/bottom_music.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'lock_music_widget.dart';
-
 class BlockedMusicWidget extends StatefulWidget {
   const BlockedMusicWidget({super.key});
 
@@ -10,189 +8,169 @@ class BlockedMusicWidget extends StatefulWidget {
 }
 
 class _BlockedMusicWidgetState extends State<BlockedMusicWidget> {
-  List<BlockMusModel> blockListMuz = [
-    BlockMusModel(
-      title: "1",
-      cicle: Container(
-        width: 78,
-        height: 78,
-        decoration: BoxDecoration(
-          borderRadius: const BorderRadius.all(
-            Radius.circular(59),
-          ),
-          border: Border.all(color: const Color(0xff8e9fcc), width: 1),
-        ),
-      ),
-    ),
-    BlockMusModel(
-      title: "1",
-      cicle: Container(
-        width: 78,
-        height: 78,
-        decoration: BoxDecoration(
-          borderRadius: const BorderRadius.all(
-            Radius.circular(59),
-          ),
-          border: Border.all(color: const Color(0xff8e9fcc), width: 1),
-        ),
-      ),
+  List<LockModel> lockMusic = [
+    LockModel(
+      title: "music title",
+      cicle: "",
     ),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Stack(
-        children: [
-          SizedBox(
-            width: double.infinity,
-            height: 250,
-            child: GridView.builder(
-                itemCount: blockListMuz.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                ),
-                primary: false,
-                padding: const EdgeInsets.all(10),
-                itemBuilder: (BuildContext context, int index) {
-                  children:
-                  <Widget>[
-                    Column(
-                      children: [
-                        Container(child: (blockListMuz[index].cicle)),
-                        const Text(
-                          'music title',
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Color(0xff8E9FCC),
-                          ),
+        child: Stack(children: [
+      SizedBox(
+          width: double.infinity,
+          height: 250,
+          child: GridView.builder(
+              itemCount: lockMusic.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+              ),
+              primary: false,
+              padding: const EdgeInsets.all(20),
+              itemBuilder: (BuildContext context, int index) {
+                return Column(
+                  children: [
+                    Container(
+                      width: 78,
+                      height: 78,
+                      decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(59),
                         ),
-                      ],
+                        border: Border.all(
+                            color: const Color(0xff8e9fcc), width: 1),
+                      ),
                     ),
-                    Column(
-                      children: [
-                        Container(
-                          width: 78,
-                          height: 78,
-                          decoration: BoxDecoration(
-                            borderRadius: const BorderRadius.all(
-                              Radius.circular(59),
-                            ),
-                            border: Border.all(
-                                color: const Color(0xff8e9fcc), width: 1),
-                          ),
-                        ),
-                        const Text(
-                          'long song title',
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Color(0xff8E9FCC),
-                          ),
-                        ),
-                      ],
+                    Text(
+                      lockMusic[index].title,
+                      style: const TextStyle(
+                        fontSize: 12,
+                        color: Color(0xff8E9FCC),
+                      ),
                     ),
-                    Column(
-                      children: [
-                        Container(
-                          width: 78,
-                          height: 78,
-                          decoration: BoxDecoration(
-                            borderRadius: const BorderRadius.all(
-                              Radius.circular(59),
-                            ),
-                            border: Border.all(
-                                color: const Color(0xff8e9fcc), width: 1),
-                          ),
-                        ),
-                        const Text(
-                          'very long song ...',
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Color(0xff8E9FCC),
-                          ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Container(
-                          width: 78,
-                          height: 78,
-                          decoration: BoxDecoration(
-                            borderRadius: const BorderRadius.all(
-                              Radius.circular(59),
-                            ),
-                            border: Border.all(
-                                color: const Color(0xff8e9fcc), width: 1),
-                          ),
-                        ),
-                        const Text(
-                          'music title',
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Color(0xff8E9FCC),
-                          ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Container(
-                          width: 78,
-                          height: 78,
-                          decoration: BoxDecoration(
-                            borderRadius: const BorderRadius.all(
-                              Radius.circular(59),
-                            ),
-                            border: Border.all(
-                                color: const Color(0xff8e9fcc), width: 1),
-                          ),
-                        ),
-                        const Text(
-                          'light music',
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Color(0xff8E9FCC),
-                          ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Container(
-                          width: 78,
-                          height: 78,
-                          decoration: BoxDecoration(
-                            borderRadius: const BorderRadius.all(
-                              Radius.circular(59),
-                            ),
-                            border: Border.all(
-                                color: const Color(0xff8e9fcc), width: 1),
-                          ),
-                        ),
-                        const Text(
-                          'long song title',
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Color(0xff8E9FCC),
-                          ),
-                        ),
-                      ],
-                    )
-                  ];
-                }),
-          ),
-          const LockBottomMusicWidget(),
-        ],
-      ),
-    );
+                  ],
+                );
+              }))
+      // Column(
+      //   children: [
+      //     Container(
+      //       width: 78,
+      //       height: 78,
+      //       decoration: BoxDecoration(
+      //         borderRadius: const BorderRadius.all(
+      //           Radius.circular(59),
+      //         ),
+      //         border: Border.all(
+      //             color: const Color(0xff8e9fcc), width: 1),
+      //       ),
+      //     ),
+      //     const Text(
+      //       'long song title',
+      //       style: TextStyle(
+      //         fontSize: 12,
+      //         color: Color(0xff8E9FCC),
+      //       ),
+      //     ),
+      //   ],
+      // ),
+      // Column(
+      //   children: [
+      //     Container(
+      //       width: 78,
+      //       height: 78,
+      //       decoration: BoxDecoration(
+      //         borderRadius: const BorderRadius.all(
+      //           Radius.circular(59),
+      //         ),
+      //         border: Border.all(
+      //             color: const Color(0xff8e9fcc), width: 1),
+      //       ),
+      //     ),
+      //     const Text(
+      //       'very long song ...',
+      //       style: TextStyle(
+      //         fontSize: 12,
+      //         color: Color(0xff8E9FCC),
+      //       ),
+      //     ),
+      //   ],
+      // ),
+      // Column(
+      //   children: [
+      //     Container(
+      //       width: 78,
+      //       height: 78,
+      //       decoration: BoxDecoration(
+      //         borderRadius: const BorderRadius.all(
+      //           Radius.circular(59),
+      //         ),
+      //         border: Border.all(
+      //             color: const Color(0xff8e9fcc), width: 1),
+      //       ),
+      //     ),
+      //     const Text(
+      //       'music title',
+      //       style: TextStyle(
+      //         fontSize: 12,
+      //         color: Color(0xff8E9FCC),
+      //       ),
+      //     ),
+      //   ],
+      // ),
+      // Column(
+      //   children: [
+      //     Container(
+      //       width: 78,
+      //       height: 78,
+      //       decoration: BoxDecoration(
+      //         borderRadius: const BorderRadius.all(
+      //           Radius.circular(59),
+      //         ),
+      //         border: Border.all(
+      //             color: const Color(0xff8e9fcc), width: 1),
+      //       ),
+      //     ),
+      //     const Text(
+      //       'light music',
+      //       style: TextStyle(
+      //         fontSize: 12,
+      //         color: Color(0xff8E9FCC),
+      //       ),
+      //     ),
+      //   ],
+      // ),
+      // Column(
+      //   children: [
+      //     Container(
+      //       width: 78,
+      //       height: 78,
+      //       decoration: BoxDecoration(
+      //         borderRadius: const BorderRadius.all(
+      //           Radius.circular(59),
+      //         ),
+      //         border: Border.all(
+      //             color: const Color(0xff8e9fcc), width: 1),
+      //       ),
+      //     ),
+      //     const Text(
+      //       'long song title',
+      //       style: TextStyle(
+      //         fontSize: 12,
+      //         color: Color(0xff8E9FCC),
+      //       ),
+      //     ),
+      //   ],
+      // ),
+    ]));
   }
 }
 
-class BlockMusModel {
+class LockModel {
   String title;
-  Widget cicle;
-  BlockMusModel({
+  String cicle;
+  LockModel({
     required this.title,
     required this.cicle,
   });

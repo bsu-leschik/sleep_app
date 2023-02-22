8da2c0677c0b0cada59b4fb800f2ac980a34b64e
Dmitry Zayats <zayatsdmitry93@gmail.com>
2023-02-22 23:22:39 +0300

    ToggleButtons(if done settings)

--- lib/fiveth_frame/widget/type_music.dart
+++ lib/fiveth_frame/widget/type_music.dart
@@ -36,43 +36,106 @@ class _TypeMusicState extends State<TypeMusic> {
   //   "Baby": false,
   //   "Single": false,
   // };
+  static const List<Widget> type = <Widget>[
+    Text("All"),
+    Text("Favorite"),
+    Text("Music"),
+    Text("Nature"),
+    Text("Urban"),
+    Text("Animals"),
+    Text("White noise"),
+    Text("Home"),
+    Text("Baby"),
+    Text("Single"),
+  ];
+
+  final List<bool> _selectedType = <bool>[
+    true,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ];
 
   @override
   Widget build(BuildContext context) {
     final double screenHeight = MediaQuery.of(context).size.height;
     final double screenWidth = MediaQuery.of(context).size.width;
-    Map<String, bool> activeType = true
-        ? context.read<DataTypeMusic>().type
-        : context.read<DataTypeMusic>().type;
+    // Map<String, bool> activeType = true
+    //     ? context.read<DataTypeMusic>().type
+    //     : context.read<DataTypeMusic>().type;
     return SizedBox(
       height: screenHeight * 0.085,
       width: screenWidth * 2.5,
       child: ListView.builder(
-        itemCount: activeType.length,
+        itemCount: type.length,
         scrollDirection: Axis.horizontal,
         shrinkWrap: true,
         itemBuilder: (context, index) {
-          final key = context.read<DataTypeMusic>().type.keys.elementAt(index);
-          return InkWell(
-            splashColor: Colors.transparent,
-            onTap: () {
-              context.read<DataTypeMusic>().type.forEach(
-                (k, v) {
-                  if (k == key) {
-                    activeType[k] = !activeType[k]!;
-                    context.read<DataTypeMusic>().value = 0;
-                  } else {
-                    activeType[k] = false;
-                  }
-                },
-              );
-              setState(() {});
+          // final key = context.read<DataTypeMusic>().type.keys.elementAt(index);
+          return
+              //  InkWell(
+              //   splashColor: Colors.transparent,
+              //   onTap: () {
+              //     setState(() {
+              //       for (int i = 0; i < _selectedType.length; i++) {
+              //         _selectedType[i] = i == index;
+              //       }
+              //     });
+
+              //   },
+              //   child: CategoryToogle(
+              //       title: type[index].toString(),
+              //       isSelected: _selectedType,
+              //     ),
+              // );// не удалось, ошибка в 94 строке
+              ToggleButtons(
+            onPressed: (int index) {
+              setState(() {
+                for (int i = 0; i < _selectedType.length; i++) {
+                  _selectedType[i] = i == index;
+                }
+              });
             },
-            child: CategoryToogle(
-              title: key,
-              isSelected: activeType[key]!,
+            renderBorder: true,
+            borderRadius: const BorderRadius.all(Radius.circular(59)),
+            selectedBorderColor: const Color(0xFFFFFFFF),
+            disabledBorderColor: const Color(0xFF8E9FCC),
+            selectedColor: const Color(0xFF281343),
+            fillColor: const Color(0xFFFFFFFF),
+            color: const Color(0xFF8E9FCC),
+            constraints: const BoxConstraints(
+              minHeight: 40.0,
+              minWidth: 80.0,
             ),
-          );
+            isSelected: _selectedType,
+            children: type,
+          ); // рабочий код, но цвета настроить
+          // InkWell(
+          //   splashColor: Colors.transparent,
+          //   onTap: () {
+          //     context.read<DataTypeMusic>().type.forEach(
+          //       (k, v) {
+          //         if (k == key) {
+          //           activeType[k] = !activeType[k]!;
+          //           context.read<DataTypeMusic>().value = 0;
+          //         } else {
+          //           activeType[k] = false;
+          //         }
+          //       },
+          //     );
+          //     setState(() {});
+          //   },
+          // child: CategoryToogle(
+          //   title: key,
+          //   isSelected: activeType[key]!,
+          //   ),
+          // ); тяжёлый к восприятию тоже рабочий
         },
       ),
     );
@@ -122,3 +185,153 @@ class CategoryToogle extends StatelessWidget {
     );
   }
 }
+
+// const List<Widget> fruits = <Widget>[
+//   Text('Apple'),
+//   Text('Banana'),
+//   Text('Orange')
+// ];
+
+// const List<Widget> vegetables = <Widget>[
+//   Text('Tomatoes'),
+//   Text('Potatoes'),
+//   Text('Carrots')
+// ];
+
+// const List<Widget> icons = <Widget>[
+//   Icon(Icons.sunny),
+//   Icon(Icons.cloud),
+//   Icon(Icons.ac_unit),
+// ];
+
+// void main() => runApp(const MyApp());
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   static const String _title = 'ToggleButtons Sample';
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       title: _title,
+//       home: ToggleButtonsSample(title: _title),
+//     );
+//   }
+// }
+
+// class ToggleButtonsSample extends StatefulWidget {
+//   const ToggleButtonsSample({super.key, required this.title});
+
+//   final String title;
+
+//   @override
+//   State<ToggleButtonsSample> createState() => _ToggleButtonsSampleState();
+// }
+
+// class _ToggleButtonsSampleState extends State<ToggleButtonsSample> {
+//   final List<bool> _selectedFruits = <bool>[true, false, false];
+//   final List<bool> _selectedVegetables = <bool>[false, true, false];
+//   final List<bool> _selectedWeather = <bool>[false, false, true];
+//   bool vertical = false;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final ThemeData theme = Theme.of(context);
+
+//     return Scaffold(
+//       appBar: AppBar(title: Text(widget.title)),
+//       body: Center(
+//         child: SingleChildScrollView(
+//           child: Column(
+//             mainAxisSize: MainAxisSize.min,
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: <Widget>[
+//               // ToggleButtons with a single selection.
+//               Text('Single-select', style: theme.textTheme.titleSmall),
+//               const SizedBox(height: 5),
+//               ToggleButtons(
+//                 direction: vertical ? Axis.vertical : Axis.horizontal,
+//                 onPressed: (int index) {
+//                   setState(() {
+//                     // The button that is tapped is set to true, and the others to false.
+//                     for (int i = 0; i < _selectedFruits.length; i++) {
+//                       _selectedFruits[i] = i == index;
+//                     }
+//                   });
+//                 },
+//                 borderRadius: const BorderRadius.all(Radius.circular(8)),
+//                 selectedBorderColor: Colors.red[700],
+//                 selectedColor: Colors.white,
+//                 fillColor: Colors.red[200],
+//                 color: Colors.red[400],
+//                 constraints: const BoxConstraints(
+//                   minHeight: 40.0,
+//                   minWidth: 80.0,
+//                 ),
+//                 isSelected: _selectedFruits,
+//                 children: fruits,
+//               ),
+//               const SizedBox(height: 20),
+//               // ToggleButtons with a multiple selection.
+//               Text('Multi-select', style: theme.textTheme.titleSmall),
+//               const SizedBox(height: 5),
+//               ToggleButtons(
+//                 direction: vertical ? Axis.vertical : Axis.horizontal,
+//                 onPressed: (int index) {
+//                   // All buttons are selectable.
+//                   setState(() {
+//                     _selectedVegetables[index] = !_selectedVegetables[index];
+//                   });
+//                 },
+//                 borderRadius: const BorderRadius.all(Radius.circular(8)),
+//                 selectedBorderColor: Colors.green[700],
+//                 selectedColor: Colors.white,
+//                 fillColor: Colors.green[200],
+//                 color: Colors.green[400],
+//                 constraints: const BoxConstraints(
+//                   minHeight: 40.0,
+//                   minWidth: 80.0,
+//                 ),
+//                 isSelected: _selectedVegetables,
+//                 children: vegetables,
+//               ),
+//               const SizedBox(height: 20),
+//               // ToggleButtons with icons only.
+//               Text('Icon-only', style: theme.textTheme.titleSmall),
+//               const SizedBox(height: 5),
+//               ToggleButtons(
+//                 direction: vertical ? Axis.vertical : Axis.horizontal,
+//                 onPressed: (int index) {
+//                   setState(() {
+//                     // The button that is tapped is set to true, and the others to false.
+//                     for (int i = 0; i < _selectedWeather.length; i++) {
+//                       _selectedWeather[i] = i == index;
+//                     }
+//                   });
+//                 },
+//                 borderRadius: const BorderRadius.all(Radius.circular(8)),
+//                 selectedBorderColor: Colors.blue[700],
+//                 selectedColor: Colors.white,
+//                 fillColor: Colors.blue[200],
+//                 color: Colors.blue[400],
+//                 isSelected: _selectedWeather,
+//                 children: icons,
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton.extended(
+//         onPressed: () {
+//           setState(() {
+//             // When the button is pressed, ToggleButtons direction is changed.
+//             vertical = !vertical;
+//           });
+//         },
+//         icon: const Icon(Icons.screen_rotation_outlined),
+//         label: Text(vertical ? 'Horizontal' : 'Vertical'),
+//       ),
+//     );
+//   }
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
-        initialRoute: "/",
+        initialRoute: "/fiveframe",
         routes: {
           "/": (context) => const OnBoardingWidget(),
           "/subscribe": (context) => const SubscribeWidget(),
